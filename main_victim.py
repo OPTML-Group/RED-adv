d9ac7aa024f1eb5964e6678297303315dc6e99d1
JC Liu <ljcc0930@qq.com>
2023-02-12 14:16:39 -0500

    Formatting and deletion

--- main_victim.py
+++ main_victim.py
@@ -5,15 +5,18 @@ import models
 import pruner
 import attacks
 
+
 def main():
     args = arg_parser.parse_args_victim_training()
     utils.set_seed(args.seed)
 
-    loader = datasets.CIFAR10(dir = args.dataset_dir, batch_size = args.batch_size)
-    model = models.ResNet9(num_classes = args.num_classes, kernel_size = args.kernel_size, act_func=args.act_func)
+    loader = datasets.CIFAR10(dir=args.dataset_dir, batch_size=args.batch_size)
+    model = models.ResNet9(num_classes=args.num_classes,
+                           kernel_size=args.kernel_size, act_func=args.act_func)
     model = model.cuda()
 
-    prefix = "seed{}_kernel{}_act{}_prune{}_".format(args.seed, args.kernel_size, args.act_func, args.pruning_ratio)
+    prefix = "seed{}_kernel{}_act{}_prune{}_".format(
+        args.seed, args.kernel_size, args.act_func, args.pruning_ratio)
     if args.structured_pruning:
         prefix += "struct_"
 
@@ -21,7 +24,9 @@ def main():
 
     if args.attack is not None:
         atk = attacks.get_attack_normalized(model, args.attack, args)
-        attacks.generate_attack_images(model, loader, atk, args.attack_save_dir)
+        attacks.generate_attack_images(
+            model, loader, atk, args.attack_save_dir)
+
 
 if __name__ == "__main__":
     main()
