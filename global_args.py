669828a6fb4d1775b6907cba1fae147ec66b652a
JC Liu <ljcc0930@qq.com>
2023-02-18 19:20:51 -0500

    Update global args

---
+++ global_args.py
@@ -0,0 +1,71 @@
+import os
+
+WORKSPACE2_DIR = "/localscratch2/ljcc"
+WORKSPACE_DIR = "/localscratch/ljcc"
+
+ATK_DIR = os.path.join(WORKSPACE2_DIR, "attack_img")
+MODEL_DIR = os.path.join(WORKSPACE2_DIR, "results")
+PARSING_DIR = os.path.join(WORKSPACE2_DIR, "parsing_models")
+
+GREP_DIR = os.path.join(WORKSPACE_DIR, "grep_datas")
+
+
+KERNEL_SIZES = [3, 5, 7]
+ACTIVATION_FUNCTIONS = ["relu", "tanh", "elu"]
+PRUNING_RATIOS = [0.0, 0.375, 0.625]
+
+
+ATTACKS = []
+ATTACKS += [
+    {'attack': 'pgd', "eps": 4, "alpha": 0.5},
+    {'attack': 'pgd', "eps": 8, "alpha": 1},
+    {'attack': 'pgd', "eps": 12, "alpha": 2},
+    {'attack': 'pgd', "eps": 16, "alpha": 2},
+    {'attack': 'pgdl2', "eps": 0.125, "alpha": 0.025},
+    {'attack': 'pgdl2', "eps": 0.25, "alpha": 0.05},
+    {'attack': 'pgdl2', "eps": 0.5, "alpha": 0.1},
+    {'attack': 'pgdl2', "eps": 1, "alpha": 0.2},
+    {'attack': 'pgdl2', "eps": 1.5, "alpha": 0.3},
+    {'attack': 'fgsm', "eps": 4},
+    {'attack': 'fgsm', "eps": 8},
+    {'attack': 'fgsm', "eps": 16},
+    {'attack': 'fgsm', "eps": 32},
+]
+ATTACKS += [
+    {'attack': 'square', "eps": 4, "norm": 'Linf'},
+    {'attack': 'square', "eps": 8, "norm": 'Linf'},
+    {'attack': 'square', "eps": 12, "norm": 'Linf'},
+    {'attack': 'square', "eps": 16, "norm": 'Linf'},
+    {'attack': 'square', "eps": 0.25, "norm": 'L2'},
+    {'attack': 'square', "eps": 0.5, "norm": 'L2'},
+    {'attack': 'square', "eps": 0.75, "norm": 'L2'},
+    {'attack': 'square', "eps": 1.0, "norm": 'L2'},
+    {'attack': 'autoattack', "eps": 4, "norm": 'Linf'},
+    {'attack': 'autoattack', "eps": 8, "norm": 'Linf'},
+    {'attack': 'autoattack', "eps": 12, "norm": 'Linf'},
+    {'attack': 'autoattack', "eps": 16, "norm": 'Linf'},
+    {'attack': 'autoattack', "eps": 0.25, "norm": 'L2'},
+    {'attack': 'autoattack', "eps": 0.5, "norm": 'L2'},
+    {'attack': 'autoattack', "eps": 0.75, "norm": 'L2'},
+    {'attack': 'autoattack', "eps": 1.0, "norm": 'L2'},
+    {'attack': 'cw', "cw-c": 1, "cw-kappa": 0},
+    {'attack': 'cw', "cw-c": 1, "cw-kappa": 0.1},
+    {'attack': 'cw', "cw-c": 1, "cw-kappa": 1},
+    {'attack': 'cw', "cw-c": 10, "cw-kappa": 0},
+    {'attack': 'cw', "cw-c": 10, "cw-kappa": 0.1},
+    {'attack': 'cw', "cw-c": 10, "cw-kappa": 1},
+    {'attack': 'cw', "cw-c": 100, "cw-kappa": 0},
+    {'attack': 'cw', "cw-c": 100, "cw-kappa": 0.1},
+    {'attack': 'cw', "cw-c": 100, "cw-kappa": 1},
+]
+ATTACKS += [
+    {'attack': 'zosignsgd', 'eps': 4, 'norm': 'Linf'},
+    {'attack': 'zosignsgd', 'eps': 8, 'norm': 'Linf'},
+    {'attack': 'zosignsgd', 'eps': 12, 'norm': 'Linf'},
+    # {'attack': 'zosgd', 'eps': 4, 'norm': 'Linf'},
+    # {'attack': 'zosgd', 'eps': 8, 'norm': 'Linf'},
+    # {'attack': 'zosgd', 'eps': 12, 'norm': 'Linf'},
+    # {'attack': 'bandit', 'eps': 4, 'norm': 'Linf'},
+    # {'attack': 'bandit', 'eps': 8, 'norm': 'Linf'},
+    # {'attack': 'bandit', 'eps': 12, 'norm': 'Linf'},
+]
--- run.py
+++ run.py
@@ -1,74 +1,21 @@
 from utils import run_commands
 import os
 
+import global_args as gargs
 
-workspace = "/localscratch2/ljcc"
-atk_dir = os.path.join(workspace, "attack_img")
-model_dir = os.path.join(workspace, "results")
-parsing_dir = os.path.join(workspace, "parsing_models")
+_atk_dir = gargs.ATK_DIR
+_model_dir = gargs.MODEL_DIR
+_parsing_dir = gargs.PARSING_DIR
 
-kernels = [3, 5, 7]
-acts = ["relu", "tanh", "elu"]
-ratios = [0.0, 0.375, 0.625]
+kernels = gargs.KERNEL_SIZES
+acts = gargs.ACTIVATION_FUNCTIONS
+ratios = gargs.PRUNING_RATIOS
 struct = [False, True]
 
-attacks = []
-attacks += [
-    {'attack': 'pgd', "eps": 4, "alpha": 0.5},
-    {'attack': 'pgd', "eps": 8, "alpha": 1},
-    {'attack': 'pgd', "eps": 12, "alpha": 2},
-    {'attack': 'pgd', "eps": 16, "alpha": 2},
-    {'attack': 'pgdl2', "eps": 0.125, "alpha": 0.025},
-    {'attack': 'pgdl2', "eps": 0.25, "alpha": 0.05},
-    {'attack': 'pgdl2', "eps": 0.5, "alpha": 0.1},
-    {'attack': 'pgdl2', "eps": 1, "alpha": 0.2},
-    {'attack': 'pgdl2', "eps": 1.5, "alpha": 0.3},
-    {'attack': 'fgsm', "eps": 4},
-    {'attack': 'fgsm', "eps": 8},
-    {'attack': 'fgsm', "eps": 16},
-    {'attack': 'fgsm', "eps": 32},
-]
-attacks += [
-    {'attack': 'square', "eps": 4, "norm": 'Linf'},
-    {'attack': 'square', "eps": 8, "norm": 'Linf'},
-    {'attack': 'square', "eps": 12, "norm": 'Linf'},
-    {'attack': 'square', "eps": 16, "norm": 'Linf'},
-    {'attack': 'square', "eps": 0.25, "norm": 'L2'},
-    {'attack': 'square', "eps": 0.5, "norm": 'L2'},
-    {'attack': 'square', "eps": 0.75, "norm": 'L2'},
-    {'attack': 'square', "eps": 1.0, "norm": 'L2'},
-    {'attack': 'autoattack', "eps": 4, "norm": 'Linf'},
-    {'attack': 'autoattack', "eps": 8, "norm": 'Linf'},
-    {'attack': 'autoattack', "eps": 12, "norm": 'Linf'},
-    {'attack': 'autoattack', "eps": 16, "norm": 'Linf'},
-    {'attack': 'autoattack', "eps": 0.25, "norm": 'L2'},
-    {'attack': 'autoattack', "eps": 0.5, "norm": 'L2'},
-    {'attack': 'autoattack', "eps": 0.75, "norm": 'L2'},
-    {'attack': 'autoattack', "eps": 1.0, "norm": 'L2'},
-    {'attack': 'cw', "cw-c": 1, "cw-kappa": 0},
-    {'attack': 'cw', "cw-c": 1, "cw-kappa": 0.1},
-    {'attack': 'cw', "cw-c": 1, "cw-kappa": 1},
-    {'attack': 'cw', "cw-c": 10, "cw-kappa": 0},
-    {'attack': 'cw', "cw-c": 10, "cw-kappa": 0.1},
-    {'attack': 'cw', "cw-c": 10, "cw-kappa": 1},
-    {'attack': 'cw', "cw-c": 100, "cw-kappa": 0},
-    {'attack': 'cw', "cw-c": 100, "cw-kappa": 0.1},
-    {'attack': 'cw', "cw-c": 100, "cw-kappa": 1},
-]
-attacks = [
-    # {'attack': 'zosignsgd', 'eps': 4, 'norm': 'Linf'},
-    # {'attack': 'zosignsgd', 'eps': 8, 'norm': 'Linf'},
-    # {'attack': 'zosignsgd', 'eps': 12, 'norm': 'Linf'},
-    # {'attack': 'zosgd', 'eps': 4, 'norm': 'Linf'},
-    # {'attack': 'zosgd', 'eps': 8, 'norm': 'Linf'},
-    # {'attack': 'zosgd', 'eps': 12, 'norm': 'Linf'},
-    # {'attack': 'bandit', 'eps': 4, 'norm': 'Linf'},
-    {'attack': 'bandit', 'eps': 8, 'norm': 'Linf'},
-    # {'attack': 'bandit', 'eps': 12, 'norm': 'Linf'},
-]
+_attacks = gargs.ATTACKS
 
 
-def get_atk_name(atk):
+def get_attack_name(atk):
     dir_name = []
     for key, val in atk.items():
         dir_name.append(f"{key}_{val}")
@@ -80,11 +27,11 @@ def gen_commands():
     ext = "--batch-size 512 --tensorboard"
 
     commands = []
-    for atk in attacks:
+    for atk in _attacks:
         for tp in input_types:
-            akt_name = get_atk_name(atk)
-            atk_path = os.path.join(atk_dir, akt_name)
-            output_path = os.path.join(parsing_dir, akt_name, tp)
+            akt_name = get_attack_name(atk)
+            atk_path = os.path.join(_atk_dir, akt_name)
+            output_path = os.path.join(_parsing_dir, akt_name, tp)
 
             command = f"python main_parsing.py --dataset-dir {atk_path} --input-type {tp} --save-dir {output_path} {ext}"
             commands.append(command)
@@ -95,11 +42,11 @@ def gen_commands_old():
     input_types = ["delta", "x_adv"]
 
     commands = []
-    for atk in attacks:
+    for atk in _attacks:
         for tp in input_types:
-            akt_name = get_atk_name(atk)
-            atk_path = os.path.join(atk_dir, akt_name)
-            output_path = os.path.join(parsing_dir, akt_name, tp)
+            akt_name = get_attack_name(atk)
+            atk_path = os.path.join(_atk_dir, akt_name)
+            output_path = os.path.join(_parsing_dir, akt_name, tp)
             if not os.path.exists(os.path.join(output_path, "final.pt")):
                 command = f"python old_parser.py --input_folder {atk_path} --input-type {tp} --save_folder {output_path}"
                 commands.append(command)
@@ -111,12 +58,12 @@ def gen_commands_eval_old():
 
     commands = []
     for tp in input_types:
-        for data_atk in attacks:
-            for model_atk in attacks:
-                data_atk_name = get_atk_name(data_atk)
-                atk_path = os.path.join(atk_dir, data_atk_name)
-                model_atk_name = get_atk_name(model_atk)
-                output_path = os.path.join(parsing_dir, model_atk_name, tp)
+        for data_atk in _attacks:
+            for model_atk in _attacks:
+                data_atk_name = get_attack_name(data_atk)
+                atk_path = os.path.join(_atk_dir, data_atk_name)
+                model_atk_name = get_attack_name(model_atk)
+                output_path = os.path.join(_parsing_dir, model_atk_name, tp)
                 command = f"python old_eval_parser.py --input_folder {atk_path} --input-type {tp} --save_folder {output_path}"
                 if not os.path.exists(os.path.join("/localscratch2/ljcc/test_log", f"data_{data_atk_name}___model_{model_atk_name}__{tp}.log")):
                     commands.append(command)
@@ -126,7 +73,7 @@ def gen_commands_eval_old():
 def gen_commands_victim(robust=True):
     commands = []
     # struct = [True]
-    for idx, atk in enumerate(attacks):
+    for idx, atk in enumerate(_attacks):
         for k in kernels:
             for a in acts:
                 for s in struct:
@@ -134,7 +81,7 @@ def gen_commands_victim(robust=True):
                         if r == 0.0 and s:
                             continue
                         command = f"python main_victim.py --kernel-size {k} --act-func {a} --pruning-ratio {r} --tensorboard"
-                        command += f" --save-dir {model_dir}"
+                        command += f" --save-dir {_model_dir}"
                         if s:
                             command += " --structured-pruning"
                         if robust:
@@ -143,8 +90,8 @@ def gen_commands_victim(robust=True):
                         for key, val in atk.items():
                             command += f" --{key} {val}"
 
-                        akt_name = get_atk_name(atk)
-                        atk_path = os.path.join(atk_dir, akt_name)
+                        akt_name = get_attack_name(atk)
+                        atk_path = os.path.join(_atk_dir, akt_name)
                         command += f" --attack-save-dir {atk_path}"
 
                         model_name = "seed{}_kernel{}_act{}_prune{}".format(
