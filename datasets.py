4974d9eb22728f9672657dc7b3f733ee1b390661
Yuguang Yao 姚禹光 <yaoyugua@msu.edu>
2023-02-20 20:46:47 -0500

    Dataset: TinyImageNet added (#3)

--- datasets.py
+++ datasets.py
@@ -12,6 +12,8 @@ from ffcv.transforms import RandomHorizontalFlip, Cutout, \
 from ffcv.transforms.common import Squeeze
 from ffcv.writer import DatasetWriter
 
+import os
+
 def CIFAR10(dir = '/tmp', batch_size = 512):
     datasets = {
         'train': torchvision.datasets.CIFAR10(dir, train=True, download=True),
@@ -52,6 +54,56 @@ def CIFAR10(dir = '/tmp', batch_size = 512):
 
         # Create loaders
         loaders[name] = Loader(f'{dir}/CIFAR10_{name}.beton',
+                                os_cache=True,
+                                batch_size=BATCH_SIZE,
+                                num_workers=8,
+                                order=OrderOption.RANDOM,
+                                drop_last=(name == 'train'),
+                                pipelines={'image': image_pipeline,
+                                        'label': label_pipeline})
+    return loaders
+
+def TinyImageNet(dir = '/tmp', batch_size = 512):
+    # before running this function, prepare TinyImageNet in "dir"
+    # see TinyImageNet prepartion in ./TinyImageNet
+    # please bash run.sh in ./TinyImageNet before laoding TinyImageNet
+    datasets = {x: torchvision.datasets.ImageFolder(os.path.join(dir, x)) 
+                  for x in ['train','val','test']}
+
+    for (name, ds) in datasets.items():
+        writer = DatasetWriter(f'{dir}/TinyImageNet_{name}.beton', {
+            'image': RGBImageField(),
+            'label': IntField()
+        })
+        writer.from_indexed_dataset(ds)
+
+    # Note that statistics are wrt to uin8 range, [0,255].
+    TinyImageNet_MEAN = [x * 255 for x in [0.4802, 0.4481, 0.3975]]
+    TinyImageNet_STD = [x * 255 for x in [0.2302, 0.2265, 0.2262]]
+    BATCH_SIZE = batch_size
+
+    loaders = {}
+    for name in ['train', 'test']:
+        label_pipeline: List[Operation] = [IntDecoder(), ToTensor(), ToDevice('cuda:0'), Squeeze()]
+        image_pipeline: List[Operation] = [SimpleRGBImageDecoder()]
+
+        # Add image transforms and normalization
+        if name == 'train':
+            image_pipeline.extend([
+                RandomHorizontalFlip(),
+                RandomTranslate(padding=2),
+                Cutout(8, tuple(map(int, TinyImageNet_MEAN))), # Note Cutout is done before normalization.
+            ])
+        image_pipeline.extend([
+            ToTensor(),
+            ToDevice('cuda:0', non_blocking=True),
+            ToTorchImage(),
+            Convert(torch.float16),
+            torchvision.transforms.Normalize(TinyImageNet_MEAN, TinyImageNet_STD),  # type: ignore
+        ])
+
+        # Create loaders
+        loaders[name] = Loader(f'{dir}/TinyImageNet_{name}.beton',
                                 os_cache=True,
                                 batch_size=BATCH_SIZE,
                                 num_workers=8,
