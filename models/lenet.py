e3bc96f30cc7f06bf8ac0f1d69e46762f72610e4
JC Liu <ljcc0930@qq.com>
2023-02-24 23:22:17 -0500

    Update LeNet

--- models/lenet.py
+++ models/lenet.py
@@ -1,13 +1,24 @@
-from __future__ import print_function
-import argparse
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
 
-import os
+
+def conv_block(in_channels, out_channels, kernel_size, act_func):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, 1),
+        nn.BatchNorm2d(out_channels),
+        act_func(),
+    )
+
+
+def fc_block(in_channels, out_channels, act_func):
+    return nn.Sequential(
+        nn.Linear(in_channels, out_channels),
+        act_func(),
+    )
+
+
+_act_func_dict = {"relu": nn.ReLU, "tanh": nn.Tanh,
+                  "silu": nn.SiLU, "elu": nn.ELU}
+
 
 class LeNet(nn.Module):
     def __init__(self, args):
@@ -15,42 +26,45 @@ class LeNet(nn.Module):
         self.num_conv = args.num_conv
         self.ks = args.kernel_size
         self.num_fc = args.num_fc
-        dict_act_func = {"relu": F.relu, "tanh": F.tanh, "silu": F.silu, "elu": F.elu}
-        self.act_func = dict_act_func[args.act_func]
-        self.conv1 = nn.Conv2d(1, 32, self.ks, 1)
-        self.conv2 = nn.Conv2d(32, 32, self.ks, 1)
-        self.conv3 = nn.Conv2d(32, 32, self.ks, 1)
-        self.conv4 = nn.Conv2d(32, 32, self.ks, 1)
-        self.conv5 = nn.Conv2d(32, 32, self.ks, 1)
-        self.conv6 = nn.Conv2d(32, 32, self.ks, 1)
-        self.fc1 = nn.Linear(32*(28-(self.ks-1)*self.num_conv)**2, 64)
-        self.fc5 = nn.Linear(64, 10)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 64)
-        self.conv_list = [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6]
-        self.fc_list = [self.fc1, self.fc2, self.fc3, self.fc4, self.fc5]
-        self.bn1 = nn.BatchNorm2d(32)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.bn4 = nn.BatchNorm2d(32)
-        self.bn5 = nn.BatchNorm2d(32)
-        self.bn6 = nn.BatchNorm2d(32)
-        self.shortcut = nn.Sequential()
-        self.bn_list = [self.bn1, self.bn2, self.bn3, self.bn4, self.bn5, self.bn6]
+
+        self.act_func = _act_func_dict[args.act_func]
+        size = 28 - (self.ks-1) * self.num_conv
+        layers = []
+        last_channel = 1
+        _conv_hidden_channel = 32
+        _fc_hidden_channel = 64
+
+        for _ in range(self.num_conv):
+            layers.append(conv_block(
+                last_channel, _conv_hidden_channel, self.ks, self.act_func))
+            last_channel = _conv_hidden_channel
+
+        layers.append(Flatten())
+        last_channel = last_channel * size ** 2
+
+        for _ in range(self.num_fc - 1):
+            layers.append(
+                fc_block(last_channel, _fc_hidden_channel, self.act_func))
+            last_channel = _fc_hidden_channel
+
+        layers.append(fc_block(last_channel, 10, self.act_func))
+        self.encoder = nn.Sequential(*layers)
+
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.act_func(x)
-        for i in range(2, self.num_conv+1):
-            x = self.conv_list[i-1](x)
-            x = self.bn_list[i-1](x)
-            x = self.act_func(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.act_func(x)
-        for i in range(2, self.num_fc):
-            x = self.fc_list[i-1](x)
-            x = self.act_func(x)
-        x = self.fc5(x)
+        x = self.encoder(x)
         return x
+
+
+class Flatten(nn.Module):
+    def forward(self, x): return x.view(x.size(0), -1)
+
+# if __name__ == "__main__":
+#     class Args: pass
+#     args = Args()
+#     args.num_conv = 2
+#     args.kernel_size = 5
+#     args.num_fc = 3
+#     args.act_func = "relu"
+#     model = LeNet(args)
+#     a = model.forward(torch.zeros([5, 1, 28, 28]))
+#     print(a.shape)
