8bb9081b024732db2c55c073a8a4f033f8fa9ad7
JC Liu <ljcc0930@qq.com>
2023-03-03 22:34:37 -0500

    Add confusion matrix

---
+++ evaluation.py
@@ -0,0 +1,152 @@
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import torch
+
+import attr_models
+import global_args as gargs
+
+
+class EvaluateParsing:
+    def __init__(self, dataset, arch, atk_name, attr_arch, setting, input_type):
+        self.setting = setting
+        self.arch = arch
+        self.input_type = input_type
+        self.attr_arch = attr_arch
+        self.atk_name = atk_name
+        self.dataset = dataset
+        self.data_arch = f"{dataset}_{arch}"
+
+    def get_attr_model(self):
+        suffix = "best"
+        self.denoiser = None
+        attr_model_dir = os.path.join(
+            gargs.PARSING_DIR, self.attr_arch, self.data_arch,
+            self.setting, self.atk_name, self.input_type)
+
+        if self.input_type == "denoise":
+            suffix = "final"
+            attr_denoiser_path = os.path.join(
+                attr_model_dir, f"denoiser_{suffix}.pt")
+            denoiser = attr_models.DnCNN(
+                image_channels=3, depth=17, n_channels=64).cuda()
+            denoiser.load_state_dict(torch.load(attr_denoiser_path))
+
+        attr_model_path = os.path.join(attr_model_dir, f"{suffix}.pt")
+
+        self.n_class = 3
+        self.n_output = 3
+        attr_model = attr_models.get_model(
+            name=self.attr_arch,
+            num_channel=gargs.DATASET_NUM_CHANNEL[self.dataset],
+            num_class=self.n_class,
+            num_output=self.n_output,
+            img_size=gargs.DATASET_INPUT_SIZE[self.dataset]).cuda()
+        print(f"Load from {attr_model_path}")
+        attr_model.load_state_dict(torch.load(attr_model_path))
+        self.attr_model = attr_model
+
+    def predict_attr_batch(self, input):
+        if self.denoiser:
+            self.denoiser.eval()
+            attr_input = input - self.denoiser(input)
+        else:
+            attr_input = input
+        self.attr_model.eval()
+        return self.attr_model(attr_input).argmax(-2).cpu()
+
+    def predict_attr(self, inputs):
+        dataset = torch.utils.data.TensorDataset(inputs.cuda())
+        dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=2048, shuffle=False)
+        pred_labels = []
+        for (x, ) in dataloader:
+            pred_labels.append(self.predict_attr_batch(x))
+        pred_labels = torch.cat(pred_labels, axis=0)
+        return pred_labels
+
+    def load_grep_data(self, grep_dir=None):
+        if grep_dir is None:
+            grep_dir = os.path.join(
+                gargs.GREP_DIR, self.data_arch, self.setting, self.atk_name)
+
+        input_name = "delta" if self.input_type == "delta" else "x_adv"
+        inputs = torch.load(os.path.join(grep_dir, f"{input_name}_test.pt"))
+        attr_labels = torch.load(os.path.join(grep_dir, f"attr_labels_test.pt"))
+
+        return inputs, attr_labels
+
+    def get_confusion(self, data_dir=None):
+        inputs, labels = self.load_grep_data(data_dir)
+        preds = self.predict_attr(inputs)
+        shape_pred = preds.numpy().max(axis=0) + 1
+        shape_labels = labels.numpy().max(axis=0) + 1
+
+        cnt_pred = 0
+        cnt_label = 0
+        for i in range(len(shape_pred)):
+            cnt_pred = cnt_pred * shape_pred[i] + preds[:, i]
+        for i in range(len(shape_labels)):
+            cnt_label = cnt_label * shape_labels[i] + labels[:, i]
+        
+        confusion = np.zeros([np.prod(shape_labels), np.prod(shape_pred)])
+        for pred, label in zip(cnt_pred, cnt_label):
+            confusion[label, pred] += 1
+        self.confusion = confusion
+
+    def pre_process(self):
+        self.get_attr_model()
+
+    def save(self, save_dir):
+        os.makedirs(save_dir, exist_ok=True)
+        plt.clf()
+        sns.heatmap(self.confusion)
+        plt.savefig(os.path.join(save_dir, "confusion" + ".png"),
+                    bbox_inches='tight', dpi=300)
+    
+    def main(self):
+        self.pre_process()
+        self.get_confusion()
+
+if __name__ == "__main__":
+    save_dir = "./figs/confusion_matrix"
+    # import shutil
+    # shutil.rmtree(save_dir, ignore_errors=True)
+    dataset = "cifar10"
+    arch = "resnet9"
+    atk_name = "attack_pgd_eps_8_alpha_1"
+    attr_arch = "conv4"
+    setting = "origin"
+    input_type = "delta"
+    eval = EvaluateParsing(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+
+    setting = "robust"
+    eval = EvaluateParsing(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+    setting = "origin"
+
+    input_type = "x_adv"
+    eval = EvaluateParsing(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+
+    input_type = "delta"
+    atk_name = "attack_fgsm_eps_8"
+    eval = EvaluateParsing(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+
+    atk_name = "attack_zosignsgd_eps_8_norm_Linf"
+    eval = EvaluateParsing(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+
+    atk_name = "attack_pgd_eps_4_alpha_0.5"
+    eval = EvaluateParsing(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
--- transfer_attack.py
+++ transfer_attack.py
@@ -5,25 +5,16 @@ import matplotlib.pyplot as plt
 
 import torch
 import torch.utils.data
-from torch.cuda.amp import autocast
 
 import global_args as gargs
 import models
 import pruner
-import attr_models
 import training_utils
 
+import evaluation
 
-class TransferAttack:
-    def __init__(self, dataset, arch, atk_name, attr_arch, setting, tp):
-        self.setting = setting
-        self.arch = arch
-        self.tp = tp
-        self.attr_arch = attr_arch
-        self.atk_name = atk_name
-        self.dataset = dataset
-        self.data_arch = f"{dataset}_{arch}"
 
+class TransferAttack(evaluation.EvaluateParsing):
     def get_model_lists(self):
         ks = gargs.KERNEL_SIZES
         acts = gargs.ACTIVATION_FUNCTIONS
@@ -39,28 +30,12 @@ class TransferAttack:
                 for idx_p, prune in enumerate(prunes):
                     model_name = training_utils.get_model_name(
                         2, k, act, prune, robust=robust)
-                    self.model_names.append(((idx_k, idx_a, idx_p), model_name))
+                    self.model_names.append(
+                        ((idx_k, idx_a, idx_p), model_name))
                     self.model_setting[model_name] = (k, act, prune)
                     self.name_mapping[(idx_k, idx_a, idx_p)] = model_name
 
-    def get_attr_model(self):
-        attr_model_path = os.path.join(
-            gargs.PARSING_DIR, self.attr_arch, self.data_arch,
-            self.setting, self.atk_name, self.tp, "best.pt")
-
-        self.n_class = 3
-        self.n_output = 3
-        attr_model = attr_models.get_model(
-            name=self.attr_arch,
-            num_channel=gargs.DATASET_NUM_CHANNEL[self.dataset],
-            num_class=self.n_class,
-            num_output=self.n_output,
-            img_size=gargs.DATASET_INPUT_SIZE[self.dataset]).cuda()
-        print(f"Load from {attr_model_path}")
-        attr_model.load_state_dict(torch.load(attr_model_path))
-        self.attr_model = attr_model
-
-    def get_model(self, model_name):
+    def get_victim_model(self, model_name):
         k, act, prune = self.model_setting[model_name]
 
         class Arg:
@@ -72,8 +47,8 @@ class TransferAttack:
         model = models.get_model(arch, args).cuda()
         return model
 
-    def load_model(self, model_name):
-        model = self.get_model(model_name)
+    def load_victim_model(self, model_name):
+        model = self.get_victim_model(model_name)
 
         last_epoch = 100 if self.dataset == "tinyimagenet" else 75
 
@@ -102,10 +77,10 @@ class TransferAttack:
         idxs = succ * corr
 
         x_adv = x_adv[idxs].contiguous()
-        input = x_adv if self.tp == "x_adv" else delta[idxs].contiguous()
+        input = delta[idxs].contiguous() if self.input_type == "delta" else x_adv
 
         self.attr_model.eval()
-        pred = self.attr_model(input.cuda()).argmax(-2).cpu()
+        pred = self.predict_attr(input.cuda()).cpu()
 
         datas = (x_adv, target[idxs].contiguous(), pred)
 
@@ -137,18 +112,18 @@ class TransferAttack:
         n_item = len(self.model_names)
         results = np.zeros([n_item, n_item, 3])
         for idx_model, (model_label, model_name) in enumerate(self.model_names):
-            model = self.load_model(model_name)
+            model = self.load_victim_model(model_name)
             for idx_data, (data_label, data_name) in enumerate(self.model_names):
                 asr, succ_rate, transfer_rate = self.transfer_attack_single(
                     model, model_label, data_label, data_name)
-                results[idx_data, idx_model] = np.array((asr, succ_rate, transfer_rate))
+                results[idx_data, idx_model] = np.array(
+                    (asr, succ_rate, transfer_rate))
 
         self.attack_results = results
 
     def pre_process(self):
+        super().pre_process()
         self.get_model_lists()
-        self.get_attr_model()
-        # self.load_grep_datas()
         self.load_datas()
 
     def main(self):
@@ -165,8 +140,9 @@ class TransferAttack:
             plt.ylabel("Origin Victim Model", fontsize=13)
             plt.xlabel("Transffered Victim Model", fontsize=13)
             # plt.xticks(rotation=45, ha='right')
-            plt.savefig(os.path.join(save_dir, name + ".png"), bbox_inches='tight', dpi=300)
-        
+            plt.savefig(os.path.join(save_dir, name + ".png"),
+                        bbox_inches='tight', dpi=300)
+
         xs, ys, names = [], [], []
         for i, name in enumerate([None, "Parsed as Origin Victim Model", "Parsed as Transferred Victim Model"]):
             if i == 0:
@@ -188,51 +164,55 @@ class TransferAttack:
         ys = np.concatenate(ys, axis=0) * 100
         names = np.array(names)
         plt.clf()
-        plt.plot([-10, 110], [-10, 110], linestyle='--', color = 'black')
+        plt.plot([-10, 110], [-10, 110], linestyle='--', color='black')
         x_name = "Parsing Accuracy on Victim Model(%)"
         y_name = "Ratio of Parsing Result(%)"
         legend_name = "Parsing Result Type"
-        sns.scatterplot(data={x_name: xs, y_name: ys, legend_name: names}, x=x_name, y=y_name, hue=legend_name, style=legend_name)
+        sns.scatterplot(data={x_name: xs, y_name: ys, legend_name: names},
+                        x=x_name, y=y_name, hue=legend_name, style=legend_name)
         plt.xlim(-5, 105)
         plt.ylim(-5, 105)
-        plt.savefig(os.path.join(save_dir, "correlation" + ".png"), bbox_inches='tight', dpi=300)
+        plt.savefig(os.path.join(save_dir, "correlation" + ".png"),
+                    bbox_inches='tight', dpi=300)
 
 
 if __name__ == "__main__":
+    save_dir = "./figs/transfer_attacks"
+    # import shutil
+    # shutil.rmtree(save_dir, ignore_errors=True)
     dataset = "cifar10"
     arch = "resnet9"
     atk_name = "attack_pgd_eps_8_alpha_1"
     attr_arch = "conv4"
     setting = "origin"
+    input_type = "delta"
+    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+
     setting = "robust"
-    tp = "delta"
-    # eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, tp)
-    # eval.main()
-    # eval.save(os.path.join("./figs/transfer_attacks", atk_name, setting))
-    
-    setting = "robust"
-    # eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, tp)
-    # eval.main()
-    # eval.save(os.path.join("./figs/transfer_attacks", atk_name, setting))
+    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
     setting = "origin"
 
-    tp = "x_adv"
-    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, tp)
+    input_type = "x_adv"
+    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, input_type)
     eval.main()
-    eval.save(os.path.join("./figs/transfer_attacks", atk_name, setting, tp))
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
 
-    tp = "delta"
+    input_type = "delta"
     atk_name = "attack_fgsm_eps_8"
-    # eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, tp)
-    # eval.main()
-    # eval.save(os.path.join("./figs/transfer_attacks", atk_name, setting))
+    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
 
     atk_name = "attack_zosignsgd_eps_8_norm_Linf"
-    # eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, tp)
-    # eval.main()
-    # eval.save(os.path.join("./figs/transfer_attacks", atk_name, setting))
-    
+    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
+
     atk_name = "attack_pgd_eps_4_alpha_0.5"
-    # eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, tp)
-    # eval.main()
-    # eval.save(os.path.join("./figs/transfer_attacks", atk_name, setting))
+    eval = TransferAttack(dataset, arch, atk_name, attr_arch, setting, input_type)
+    eval.main()
+    eval.save(os.path.join(save_dir, atk_name, setting, input_type))
